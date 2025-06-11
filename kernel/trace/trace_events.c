6caaff7b297172c3a4937e056ff4f261cbf79db3
Gabriele Paoloni <gpaoloni@redhat.com>
2025-06-11 15:46:18 +0200

    tracing: added documentation for event_enable_read and improved event_enable_write
    Signed-off-by: Gabriele Paoloni <gpaoloni@redhat.com>

--- kernel/trace/trace_events.c
+++ kernel/trace/trace_events.c
@@ -1849,6 +1849,48 @@ static void p_stop(struct seq_file *m, void *p)
 	mutex_unlock(&event_mutex);
 }
 
+/**
+ * SPDX-Req-ID: [TODO automatically generate it]
+ * SPDX-Req-Text:
+ * event_enable_read - read from a trace event file to retrieve its status.
+ * @filp: file pointer associated with the target trace event;
+ * @ubuf: user space buffer where the event status is copied to;
+ * @cnt: number of bytes to be copied to the user space buffer;
+ * @ppos: the current position in the buffer.
+ *
+ * This is a way for user space executables to retrieve the status of a
+ * specific event
+ *
+ * Function's expectations:
+ * - This function shall lock the global event_mutex before performing any
+ *   operation on the target event file and unlock it after all operations on
+ *   the target event file have completed;
+ * - This function shall retrieve the status flags from the file associated
+ *   with the target event;
+ * - This function shall format the string to report the event status to user
+ *   space:
+ *   - The first character of the string to be copied to user space shall be
+ *     set to "1" if the enabled flag is set AND the soft_disabled flag is not
+ *     set, else it shall be set to "0";
+ *   - The second character of the string to be copied to user space shall be
+ *     set to "*" if either the soft_disabled flag or the soft_mode flag is
+ *     set, else it shall be set to "\n";
+ *   - The third character of the string to be copied to user space shall b
+ *     set to "\n" if either the soft_disabled flag or the soft_mode flag is
+ *     set, else it shall be set to "0";
+ *   - Any other character in the string to be copied to user space shall be
+ *     set to "0";
+ * - This function shall check if the requested cnt bytes are equal or greater
+ *   than the length of the string to be copied to user space (else a
+ *   corrupted event status could be reported);
+ * - This function shall invoke simple_read_from_buffer() to perform the copy
+ *   of the kernel space string to ubuf.
+ *
+ * Returns the number of copied bytes on success, -ENODEV if the event file
+ * cannot be retrieved from the input filp, -EINVAL if cnt is less than the
+ * length of the string to be copied to ubuf, any error returned by
+ * simple_read_from_buffer
+ */
 static ssize_t
 event_enable_read(struct file *filp, char __user *ubuf, size_t cnt,
 		  loff_t *ppos)
@@ -1892,9 +1934,10 @@ event_enable_read(struct file *filp, char __user *ubuf, size_t cnt,
  * recording.
  *
  * Function's expectations:
- * - This function shall copy the input ubuf buffer from user space to a kernel
- *   space buffer and shall convert the respective string to a decimal base
- *   format number;
+ * - This function shall copy cnt bytes from the input ubuf buffer to a kernel
+ *   space buffer (or the whole input ubuf if cnt is larger than ubuf size)
+ *   and shall convert the string within the kernel space buffer into a decimal
+ *   base format number;
  * - This function shall lock the global event_mutex before performing any
  *   operation on the target event file and unlock it after all operations on
  *   the target event file have completed;
@@ -1905,17 +1948,12 @@ event_enable_read(struct file *filp, char __user *ubuf, size_t cnt,
  * - This function shall invoke ftrace_event_enable_disable to enable or
  *   disable the target trace event according to the value read from user space
  *   (0 - disable, 1 - enable);
- * - Before returning this function shall increment ppos by the number of bytes
- *   copied from the user space ubuf buffer.
  *
  * Returns 0 on success, any error returned by kstrtoul_from_user, -ENODEV if
  * the event file cannot be retrieved from the input filp, any error returned by
  * tracing_update_buffers, any error returned by ftrace_event_enable_disable,
  * -EINVAL if the value copied from the user space ubuf is different from 0 or
  * 1.
- *
- * TODO:
- * - Why is ppos updated? It is not used at all anyway...
  */
 static ssize_t
 event_enable_write(struct file *filp, const char __user *ubuf, size_t cnt,
